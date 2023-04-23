45cc860ae565842c7a0ac5b1decc203a64960e23
Breuch-tz <99825743+Breuch-tz@users.noreply.github.com>
2023-04-23 22:50:18 +0200

    styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  public ngStyle: String = 'ngStyle2';
+  public ngStyle1: String = 'ngStyleBefore';
+  public ngStyle2: String = 'ngStyleBefore';
+
   constructor(private elementRef: ElementRef) {}
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: any) {
@@ -15,21 +17,54 @@ export class HomeComponent {
 
   private checkElementViewport() {
     if (window.innerHeight <= 880) {
-      this.ngStyle = 'ngStyle1';
+      this.ngStyle1 = 'ngStyleAfter';
+      this.ngStyle2 = 'ngStyleAfter';
       return;
     }
-    const myElement = this.elementRef.nativeElement.querySelector('#trauringe');
-    const bounding = myElement.getBoundingClientRect();
+    // const myElementA =
+    //   this.elementRef.nativeElement.querySelector('#trauringe');
+    // const myElementB = this.elementRef.nativeElement.querySelector('#test2');
+    // const boundingA = myElementA.getBoundingClientRect();
+    // const bounding = myElementB.getBoundingClientRect();
 
-    if (
-      bounding.top >= -150 &&
-      bounding.left >= 0 &&
-      bounding.right <= window.innerWidth &&
-      bounding.bottom - 150 <= window.innerHeight
-    ) {
-      this.ngStyle = 'ngStyle1';
-    } else {
-      // this.ngStyle = 'ngStyle2';
+    // const elements = [myElementA, myElementB];
+
+    // if (
+    //   bounding.top >= -150 &&
+    //   bounding.left >= 0 &&
+    //   bounding.right <= window.innerWidth &&
+    //   bounding.bottom - 150 <= window.innerHeight
+    // ) {
+    //   this.ngStyle = 'ngStyleAfter';
+    // } else {
+    //   // this.ngStyle = 'ngStyle2';
+    // }
+
+    const myElementA = this.elementRef.nativeElement.querySelector('#section1');
+    const myElementB = this.elementRef.nativeElement.querySelector('#section2');
+
+    const elements = [myElementA, myElementB];
+
+    for (let i = 0; i < elements.length; i++) {
+      const bounding = elements[i].getBoundingClientRect();
+
+      if (
+        bounding.top >= -300 &&
+        bounding.left >= 0 &&
+        bounding.right <= window.innerWidth &&
+        bounding.bottom - 300 <= window.innerHeight
+      ) {
+        switch (i + 1) {
+          case 1:
+            this.ngStyle1 = 'ngStyleAfter';
+            break;
+          case 2:
+            this.ngStyle2 = 'ngStyleAfter';
+            break;
+        }
+      } else {
+        // this.ngStyle = 'ngStyle2';
+      }
     }
   }
 }
